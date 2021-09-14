82883259102b1364fd0fac93e174b1650c8248df
Philipe Vasconcelos <philipe_99@hotmail.com>
2021-09-14 17:16:47 -0300

    Adicionar redes sociais

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar_menu')
+const navlogo = document.querySelector('#navbar_logo');
 
 //Display Mobile Menu
 
@@ -9,3 +10,30 @@ const mobileMenu = () => {
 };
 
 menu.addEventListener('click', mobileMenu);
+
+//Show active menu when scrolling
+
+const highlightMenu = () => {
+    const elem = document.querySelector('.highlight')
+    const homeMenu = document.querySelector('.#home-page')
+    const aboutMenu = document.querySelector('#about-page')
+    const servicesMenu = document.querySelector('#services-page')
+    let scrollPos = window.scrollY
+
+    //add 'highlight' class to my menu items
+    if (window.innerWidth > 960 && scrollPos < 600) {
+        homeMenu.classList.add('highlight')
+        aboutMenu.classList.remove('highlight')
+        return
+    } else if (window.innerWidth > 960 && scrollPos < 1400) {
+        aboutMenu.classList.add('highlight')
+        homeMenu.classList.remove('highlight')
+        servicesMenu.classList.remove('highlight')
+        return
+    } else if (window.innerWidth > 960 && scrollPos < 2345) {
+        aboutMenu.classList.remove('highlight')
+        homeMenu.classList.remove('highlight')
+        return
+    }
+
+}
